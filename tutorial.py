4f2d824c92c2d8524a20ed3f5c2dc1545fa40faf
Matt Keuning <keuninma@mail.gvsu.edu>
2024-12-17 14:32:31 -0500

    adding screen and closing event

--- tutorial.py
+++ tutorial.py
@@ -2,6 +2,16 @@ import pygame
 
 
 pygame.init()
+width = 800
+height = 500
+screen = pygame.display.set_mode((width,height))
 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    # draw elements
+    # update everything
+    pygame.display.update()
 
 
