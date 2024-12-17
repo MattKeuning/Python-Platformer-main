6db3e2c174e527231e6d0f6358bfce8343f9f3fe
Matt Keuning <keuninma@mail.gvsu.edu>
2024-12-17 15:48:31 -0500

    setting game title

--- tutorial.py
+++ tutorial.py
@@ -1,15 +1,17 @@
 import pygame
-
+from sys import exit
 
 pygame.init()
 width = 800
 height = 500
 screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption('Runner Game')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            exit()
     # draw elements
     # update everything
     pygame.display.update()
