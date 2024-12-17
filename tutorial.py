01149973282791b6b81513ed608993f04fcde32f
Matt Keuning <keuninma@mail.gvsu.edu>
2024-12-17 16:22:15 -0500

    setting up a red surface object

--- tutorial.py
+++ tutorial.py
@@ -6,14 +6,19 @@ width = 800
 height = 500
 screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption('Runner Game')
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100, 200))
+test_surface.fill('Red')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            pygame.quit() 
             exit()
-    # draw elements
-    # update everything
-    pygame.display.update()
+    
+    screen.blit(test_surface,(200,100))
 
+    pygame.display.update()
+    clock.tick(60)
 
