315249461d0830bf5e7772d5aa94b06d97661b72
Matt Keuning <keuninma@mail.gvsu.edu>
2024-12-19 12:02:01 -0500

    adding rectangles and collision detection

--- tutorial.py
+++ tutorial.py
@@ -13,12 +13,13 @@ sky_surface = pygame.image.load('8bit-pixel-art-night-sky-game-space-landscape-v
 ground_surface = pygame.image.load('stone.png').convert()
 text_surface = test_font.render('Matt\'s Mario', False, 'Grey')
 enemy_surface1 = pygame.image.load('snail1.png').convert_alpha()
-snail_x_pos = 700
 temp = pygame.image.load('snail2.png').convert_alpha()
-player = pygame.image.load('clipart1361415.png')
-np = pygame.transform.scale(player, (60, 100))
+player = pygame.image.load('clipart1361415.png').convert_alpha()
 npx = 100
 npy = 360
+np = pygame.transform.scale(player, (60, 100))
+player_rect = np.get_rect(topleft = (npx,npy))
+snail_rect = enemy_surface1.get_rect(topleft = (700, 420))
 jump_count = 0
 is_jump = False
 going_down = False
@@ -36,40 +37,39 @@ while True:
                     is_jump = True
 
     if is_jump and jump_count < 15:
-        npy -= 10
+        player_rect.y -= 10
         jump_count += 1
     if is_jump and jump_count >= 15:
         going_down = True
 
     if going_down:
         is_jump = False
-        if npy == 360:
+        if player_rect.y == 360:
             going_down = False
             jump_count = 0
-        npy += 5
+        player_rect.y += 5
 
         
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RIGHT]:
-        npx += 4
+        player_rect.x += 4
     if keys[pygame.K_LEFT]:
-        npx -= 4
+        player_rect.x -= 4
     
     
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,450))
     screen.blit(ground_surface,(400, 450))
-    screen.blit(temp, (snail_x_pos, 420))
+    screen.blit(temp, snail_rect)
     screen.blit(text_surface,(300, 50))
-    screen.blit(np,(npx,npy))
-    snail_x_pos -= 5
-    if snail_x_pos % 2 == 0:
-        temp = pygame.image.load('snail1.png')
-    else:
-        temp = pygame.image.load('snail2.png')
-    if snail_x_pos < -100:
-        snail_x_pos = 800
+    screen.blit(np, player_rect)
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+    collision = player_rect.colliderect(snail_rect)
+    if collision:
+        pygame.quit()
 
     pygame.display.update()
     clock.tick(60)
